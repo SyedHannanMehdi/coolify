71fbbec9b0012264442c4dddd70443e7ec81f1d4
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2026-03-28 23:07:33 +0100

    feat(workspace): scaffold workspace model and migration

---
+++ database/migrations/2026_03_28_215843_create_workspaces_table.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('workspaces', function (Blueprint $table): void {
+            $table->ulid('id')->primary();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->smallInteger('concurrent_builds');
+            $table->smallInteger('default_deployment_timeout');
+            $table->timestamps();
+        });
+    }
+};
