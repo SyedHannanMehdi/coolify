cfad38ae9701ba29bf4df67e71d3b017957b29ea
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2026-03-28 23:09:49 +0100

    feat(auth): scaffold user model and migration

---
+++ database/migrations/2026_03_28_215838_create_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table): void {
+            $table->ulid('id')->primary();
+            $table->string('name');
+            $table->string('display_name')->nullable();
+            $table->string('email')->unique();
+            $table->string('notification_email')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('notification_email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->text('two_factor_secret')->nullable();
+            $table->text('two_factor_recovery_codes')->nullable();
+            $table->timestamp('two_factor_confirmed_at')->nullable();
+            $table->timestamps();
+        });
+    }
+};
